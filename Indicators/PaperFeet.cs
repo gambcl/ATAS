ae85f28439c3bdce425d3d015b30ba527ed00db7
Charles Gamble <gambcl@proton.me>
2025-02-11 22:37:26 +0000

    Added buy/sell alerts to PaperFeet indicator

--- Indicators/PaperFeet.cs
+++ Indicators/PaperFeet.cs
@@ -2,6 +2,7 @@
 using ATAS.Indicators.Drawing;
 using OFT.Rendering.Context;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Drawing;
 
 namespace gambcl.ATAS.Indicators
@@ -31,6 +32,8 @@ namespace gambcl.ATAS.Indicators
         #region Members
 
         private bool _isRsiInitialized = false;
+        private int _lastBuySignalAlertBar = 0;
+        private int _lastSellSignalAlertBar = 0;
         private readonly ValueDataSeries _signals = new("Signals")
         {
             VisualType = VisualMode.Hide,
@@ -40,6 +43,19 @@ namespace gambcl.ATAS.Indicators
         #endregion
 
         #region Properties
+
+        [Display(Name = "Buy Signal", GroupName = "Signals", Description = "When enabled, buy signals will be highlighted in the specified color.", Order = 351)]
+        public FilterColor BuySignalColor {  get; set; }
+
+        [Display(Name = "Sell Signal", GroupName = "Signals", Description = "When enabled, sell signals will be highlighted in the specified color.", Order = 352)]
+        public FilterColor SellSignalColor { get; set; }
+
+        [Display(Name = "Buy Signal", GroupName = "Alerts", Description = "When enabled, an alert is triggered by a buy signal, using the specified sound file.", Order = 391)]
+        public FilterString BuySignalAlertFilter { get; set; }
+
+        [Display(Name = "Sell Signal", GroupName = "Alerts", Description = "When enabled, an alert is triggered by a sell signal, using the specified sound file.", Order = 392)]
+        public FilterString SellSignalAlertFilter { get; set; }
+
         #endregion
 
         #region Constructor
@@ -48,6 +64,15 @@ namespace gambcl.ATAS.Indicators
         {
             // NOTE: The DataSeries must match the order found in PaperFeetDataSeriesIndexEnum.
             DataSeries.Add(_signals);
+
+            BuySignalColor = new(true) { Enabled = true, Value = DefaultColors.Green.GetWithTransparency(50).Convert() };
+            SellSignalColor = new(true) { Enabled = true, Value = DefaultColors.Red.GetWithTransparency(50).Convert() };
+
+            BuySignalAlertFilter = new(true) { Enabled = true, Value = "alert1" };
+            SellSignalAlertFilter = new(true) { Enabled = true, Value = "alert1" };
+
+            BuySignalColor.PropertyChanged += OnSignalPropertyChanged;
+            SellSignalColor.PropertyChanged += OnSignalPropertyChanged;
 
             _isRsiInitialized = false;
         }
@@ -85,6 +110,22 @@ namespace gambcl.ATAS.Indicators
                 // Sell signal
                 _signals[bar] = -1m;
             }
+
+            // Alerts
+            if (bar == (CurrentBar - 1) && InstrumentInfo is not null)
+            {
+                if (BuySignalColor.Enabled && BuySignalAlertFilter.Enabled && (bar != _lastBuySignalAlertBar) && (_signals[bar] > 0))
+                {
+                    AddAlert(BuySignalAlertFilter.Value, InstrumentInfo.Instrument, $"BUY SIGNAL: Laguerre RSI leaving oversold region {this[bar]:0.#####}", DefaultColors.Black.Convert(), BuySignalColor.Value);
+                    _lastBuySignalAlertBar = bar;
+                }
+
+                if (SellSignalColor.Enabled && SellSignalAlertFilter.Enabled && (bar != _lastSellSignalAlertBar) && (_signals[bar] < 0))
+                {
+                    AddAlert(SellSignalAlertFilter.Value, InstrumentInfo.Instrument, $"SELL SIGNAL: Laguerre RSI leaving overbought region {this[bar]:0.#####}", DefaultColors.Black.Convert(), SellSignalColor.Value);
+                    _lastSellSignalAlertBar = bar;
+                }
+            }
         }
 
         protected override void OnRender(RenderContext context, DrawingLayouts layout)
@@ -97,8 +138,11 @@ namespace gambcl.ATAS.Indicators
             if (Container is null)
                 return;
 
-            Color buyColor = DefaultColors.Green.GetWithTransparency(50);
-            Color sellColor = DefaultColors.Red.GetWithTransparency(50);
+            if (!BuySignalColor.Enabled && !SellSignalColor.Enabled)
+                return;
+
+            Color buyColor = BuySignalColor.Value.Convert();
+            Color sellColor = SellSignalColor.Value.Convert();
             int topY = Container.Region.Top;
             int bottomY = Container.Region.Bottom;
             int height = bottomY - topY;
@@ -108,7 +152,7 @@ namespace gambcl.ATAS.Indicators
                 bool buySignal = _signals[i] > 0;
                 bool sellSignal = _signals[i] < 0;
 
-                if (buySignal || sellSignal)
+                if ((buySignal && BuySignalColor.Enabled) || (sellSignal && SellSignalColor.Enabled))
                 {
                     int leftX = ChartInfo.GetXByBar(i, true) - 1;
                     int rightX = ChartInfo.GetXByBar(i + 1, true) - 1;
@@ -118,6 +162,15 @@ namespace gambcl.ATAS.Indicators
                     context.FillRectangle(buySignal ? buyColor : sellColor, signalRect);
                 }
             }
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void OnSignalPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            RecalculateValues();
         }
 
         #endregion
